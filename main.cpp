9b5114637ec2ed5fd276f1cf4245ca7da4613453
MaggiePeterson <margaret.peterson@gmail.com>
2019-04-19 21:32:31 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Metrics
+//
+//  Created by Margaret Peterson on 4/19/19.
+//  Copyright © 2019 Margaret Peterson. All rights reserved.
+//
+
+#include <iostream>
+#include "Metrics.hpp"
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}

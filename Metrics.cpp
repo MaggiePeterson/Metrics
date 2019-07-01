1034f92a71877fa684d696ac57f6e83d06a2ef36
MaggiePeterson <margaret.peterson@gmail.com>
2019-06-30 21:53:23 -0700

    gets angle and distance

--- Metrics.cpp
+++ Metrics.cpp
@@ -8,40 +8,26 @@
 
 #include "Metrics.hpp"
 
-struct x_target {
-   int start =0;
-   int end =0;
-};
-
-struct y_target {
-   int start =0;
-   int end =0;
-};
-
-struct target {
-   y_target y;
-   x_target x;
-};
-
 //constructor
-Metrics::Metrics(int frameWidth, int degree){
-   this->frameW = frameWidth;
-   this->FOV = degree;
-   vector<float>radiusX = {0};
-   vector<float>distY = {0};
+Metrics::Metrics(int frameWidth, int FOV){
+   this->frame_width = frameWidth;
+   this->field_of_view = FOV;
 }
 
-Mat Metrics:: calculate(Mat *img){
+void Metrics:: TargetInit(Mat *img){
 
    int haswhite = 0;
    int prevhaswhite = 0;
-   vector<x_target> xt;
-   vector<y_target> yt;
-   struct x_target x;
-   struct y_target y;
-   vector<target> targets;
-   target vision_target;
+   int target_index = 0;
+   vector<VerticalLine>vertical_lines;
+   vector<HorizontalLine>horizontal_lines;
+   vector<Target>target_list;
+   VerticalLine x;
+   HorizontalLine y;
+   Target vision_target;
+   TargetPair pair;
 
+   pair_list.clear();
 
    //vertical
    for(int i =0; i < img->cols; i++){
@@ -55,21 +41,20 @@ Mat Metrics:: calculate(Mat *img){
       }
 
       if((haswhite>=15) && !prevhaswhite){   //if current is white but prev is black
-         line(*img, Point(i, 0), Point(i,480), Scalar(250,250,250),1, LINE_8, 0); //draw line
          prevhaswhite =1;
          x.start = i;
       }
       else if ((haswhite<=15) && prevhaswhite){
-         line(*img, Point(i, 0), Point(i,480), Scalar(250,250,250),1, LINE_8, 0); //draw line
          prevhaswhite =0;
 
-         x.end = i;
-         xt.push_back(x);
+         x.end =i;
+         vertical_lines.push_back(x);
 
       }
    }
 
    prevhaswhite = 0;
+   target_index = 0;
 
    //horizontal
    for(int j =0; j< img->rows; j++){
@@ -83,93 +68,145 @@ Mat Metrics:: calculate(Mat *img){
       }
 
       if((haswhite>=5) && !prevhaswhite){   //if current is white but prev is black
+
          line(*img, Point(0, j), Point(640,j), Scalar(250,250,250),1, LINE_8, 0); //draw line
          prevhaswhite =1;
 
          y.start = j;
-
       }
       else if ((haswhite<=5) && prevhaswhite){
+
          line(*img, Point(0, j), Point(640,j), Scalar(250,250,250),1, LINE_8, 0); //draw line
          prevhaswhite =0;
 
          y.end = j;
-         yt.push_back(y);
-         yt.push_back(y);
+         horizontal_lines.push_back(y);
+         horizontal_lines.push_back(y);
+
       }
    }
 
-   for(int i = 0; i<xt.size(); i++){
-      cout<<i+1 <<" x target: "<<xt[i].end<<" "<<xt[i].start<<endl;
-      cout<<i+1 <<" y target: "<<yt[i].end<<" "<<yt[i].start<<endl; //only 1
+   //group bounding lines to individual vision targets
+   for(int i =0; i< vertical_lines.size(); i++){
 
-      vision_target.x = xt[i];
-      vision_target.y = yt[i];
+      vision_target.x_line = vertical_lines[i];
+      vision_target.y_line = horizontal_lines[i];
+      vision_target.height = vision_target.y_line.end - vision_target.y_line.start;
+      vision_target.width = vision_target.x_line.end - vision_target.x_line.start;
+      vision_target.HW_Ratio = double(vision_target.height)/double(vision_target.width);
 
-      cout<<i+1 <<" x Vtarget: "<<vision_target.x.end<<" "<<vision_target.x.start<<endl;
-      cout<<i+1 <<" y Vtarget: "<<vision_target.y.end<<" "<<vision_target.y.start<<endl; //only 1
+      target_list.push_back(vision_target);
 
    }
 
+   //find target pairs by comparing to ratio
+   for(int i=0; i< target_list.size(); i++){
 
-   return *img;
+      if(target_list[i].HW_Ratio - target_list[i+1].HW_Ratio <= TargetPair::TOLERANCE){
+         pair.target1 =target_list[i]; //add target to pair
+         pair.target2 =target_list[i+1];
+         pair_list.push_back(pair);
+
+         i++; //skip checking next target
+
+      }
+   }
+
+   if(pair_list.size() < 1){
+      cout<<"ERROR: no pairs found"<<endl;
+   }
+
 }
 
-void Metrics:: calibrateZero(Mat *img, float dist){
-   cout<<"METRICS: Calibrating to Zero; dist: "<<dist<<endl;
-   //  drawBoundingBox(img);
-   this->radiusX.push_back(radius1);
-   this->radiusX.push_back(radius2);
-   this->distY.push_back(0);
-   this->distY.push_back(0);
+void Metrics:: calibrateZero(Mat *img, double dist){
 
-   this->zeroDist = dist;
+   target_pair_length.push_back(pair_list[findClosetTarget()].length);
+   this->distance.push_back(dist);
+
+   calibration = dist;
 }
 
-void Metrics::configValues(Mat *img, float dist){
-   cout<<"METRICS: Config; dist: "<<dist<<endl;
-   //   drawBoundingBox(img);
-   this->radiusX.push_back(radius1);
-   this->radiusX.push_back(radius2);
-   this->distY.push_back(dist);
-   this->distY.push_back(dist);
+void Metrics::configValues(Mat *img, double dist){
+
+   target_pair_length.push_back(pair_list[findClosetTarget()].length);
+   this->distance.push_back(dist);
 }
 
-float Metrics:: lineOfRegression(float rad1, float rad2){
+int Metrics::findClosetTarget(){
 
-   if(this->radiusX.size() != this->distY.size()){
+   // find closest target by getting largest length
+   int i = 0;
+   int temp = i;
+   int max = 0;
+
+   while(i< pair_list.size())
+   {
+      if (max < pair_list[i].length)
+      {
+         max = pair_list[i].length;
+         temp = i;
+      }
+      i++;
+   }
+
+   return temp;
+}
+
+int Metrics::findClosestTargetLength(){
+
+   int max = 0;
+   for (TargetPair pair : pair_list){
+      if(pair.length > max)
+         max = pair.length;
+   }
+   return max;
+}
+
+int Metrics:: getAngle(){
+
+   int index = findClosetTarget();
+
+   pair_list[index].center = (pair_list[index].target1.x_line.start + pair_list[index].target2.x_line.end)/2;
+
+   int angle = field_of_view * abs((frame_width/2) - pair_list[index].center)/frame_width;
+
+   return angle;
+}
+
+double Metrics::getDistance(int length){
+
+   if(target_pair_length.size() != distance.size()){
       cerr<< "ERROR: Vectors are not the same size"<<endl;
    }
-   float n = this->radiusX.size();
+   double n = target_pair_length.size();
 
-   float avgX = accumulate(this->radiusX.begin(), this->radiusX.end(), 0.0) / n;
-   float avgY = accumulate(this->distY.begin(), this->distY.end(), 0.0) / n;
+   double avgX = accumulate(target_pair_length.begin(), target_pair_length.end(), 0.0) / n;
+   double avgY = accumulate(distance.begin(), distance.end(), 0.0) / n;
 
-   float numerator = 0.0;
-   float denominator = 0.0;
+   double numerator = 0.0;
+   double denominator = 0.0;
 
    for(int i=0; i<n; ++i){
-      numerator += (this->radiusX[i] - avgX) * (this->radiusX[i] - avgY);
-      denominator += (this->radiusX[i] - avgX) * (this->radiusX[i] - avgX);
+      numerator += (target_pair_length[i] - avgX) * (target_pair_length[i] - avgY);
+      denominator += (target_pair_length[i] - avgX) * (target_pair_length[i] - avgX);
    }
 
    if(denominator==0){
       cerr << "ERROR: Undefined slope"<<endl;
    }
 
-   float radius = (rad1+rad2)/2; //average radius
-   float slope = numerator / denominator;
-   float dist = slope*radius + this->zeroDist;
+   double slope = numerator / denominator;
+   double dist = slope*length + calibration;
    return dist;
 }
 
-float Metrics::distance(){
+string Metrics::getAngleAndDistance(){
 
-   return lineOfRegression(radius1, radius2);
-}
+   ostringstream oss;
 
-int Metrics:: angle(){
-   return this->angl;
+   oss<< getAngle() <<" "<<getDistance(findClosestTargetLength());    //save data to string
+   return oss.str();
+
 }
 
 void Metrics:: writeMetrics(string textFile){
@@ -177,9 +214,9 @@ void Metrics:: writeMetrics(string textFile){
    ofstream myFile;
    myFile.open(textFile);
 
-   myFile << radiusX.size() <<endl;
-   for(int i =0; i< this->radiusX.size(); i++){
-      myFile<< this->radiusX[i] <<endl<<  this->distY[i] << endl;
+   myFile << target_pair_length.size() <<endl;
+   for(int i =0; i< target_pair_length.size(); i++){
+      myFile<< target_pair_length[i] <<endl<<  distance[i] << endl;
    }
    myFile.close();
 
@@ -191,10 +228,10 @@ bool Metrics:: readMetrics(string textFile){
    inFile.open(textFile);
    int size;
    if(inFile>> size){
-      radiusX.resize(size);
-      distY.resize(size);
+      target_pair_length.resize(size);
+      distance.resize(size);
       for(int i=0; i< size; i++){
-         inFile >> this->radiusX[i] >> this->distY[i];
+         inFile >> target_pair_length[i] >> distance[i];
       }
       return true;
    }
